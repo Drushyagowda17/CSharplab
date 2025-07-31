d5a56c1c92b7937e772c85b4d873d7554e0b0027
Drushyagowda17 <drushyagmgowda@gmail.com>
2025-07-31 14:49:08 +0530

    Add files via upload

---
+++ PNZ/Pnz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNZ
+{
+    internal class Pnz
+    {
+        int num;
+        public Pnz()
+        {
+            num = 2;
+        }
+        public Pnz(int num)
+        {
+            this.num = num;
+        }
+        public void check()
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("number is zero");
+            }
+            else if (num > 1)
+            {
+                Console.WriteLine("number is positive");
+
+            }
+            else
+            {
+                Console.WriteLine("its negative");
+            }
+        }
+    }
+}
---
+++ PNZ/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PNZ
+{
+    public class Program
+    {
+       public static int num;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+           num = Convert.ToInt32(Console.ReadLine());
+            Pnz n = new Pnz(num);
+            n.check();
+        }
+    }
+}
