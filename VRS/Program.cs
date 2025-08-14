4a782d4dd83689b221d965ed903158df49560d1d
Drushyagowda17 <drushyagmgowda@gmail.com>
2025-08-14 15:10:56 +0530

    initial commit

---
+++ VRS/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VRS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Vehicle("KTM", "DUKE 360", 69);
+            v.display();
+            Vehicle c = new Car("MAHENDRA", "THAR", 699, 5, "Petrol");
+            c.display();
+        }
+    }
+}
