80474d328d01b528e38f324b76d7dcf1ed6be4cd
Drushyagowda17 <drushyagmgowda@gmail.com>
2025-08-28 14:13:47 +0530

    Add files via upload

---
+++ Store/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product("Pillow", 100.50);
+            p.Display();
+            EProduct e = new EProduct("Dryer", 499.99, "Sony", 2);
+            e.Display();
+            Smartphone s = new Smartphone("Smartphone", 50000, "google pixle", 1, "android");
+            s.Display();
+        }
+    }
+}
