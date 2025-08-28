8d3f171b829df6d07b7548dcec3fcc506053b639
Drushyagowda17 <drushyagmgowda@gmail.com>
2025-08-28 15:08:14 +0530

    Add files via upload

---
+++ PPS/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PPS
+{
+    using System;
+
+    namespace Store
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Payment creditCardPayment = new CreditCardPayment(1500, DateTime.Now, "564987654", "rahul");
+                creditCardPayment.ProcessPayment();
+
+                Payment upiPayment = new UPIPayment(400, DateTime.Now, "rahul99.upi", "State  Bank");
+                upiPayment.ProcessPayment();
+            }
+        }
+    }
+
+}
